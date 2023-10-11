b1381fa308ad2589d9005b62daab90470a93571c
Emilio Wuerges <wuerges@gmail.com>
2023-10-11 13:13:34 -0300

    working!

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,6 @@ fn Controlled(
             (Some(active), Some(node)) => active == ***node,
             _ => false,
         };
-        // let is_focused = true;
-        // console_log(&format!("el: {} active: {}", label, is_focused));
 
         let local = local.get();
         let value = value.get().to_string();
@@ -84,111 +82,60 @@ fn App() -> impl IntoView {
     let (amount, set_amount) = create_signal::<f64>(0.0);
     let (yearly, set_yearly) = create_signal::<f64>(0.0);
 
-    // let (daily, set_daily) = create_signal("".to_string());
-    // let (monthly, set_monthly) = create_signal("".to_string());
-    // let (yearly_5, set_yearly_5) = create_signal("".to_string());
-    // let (yearly_10, set_yearly_10) = create_signal("".to_string());
+    let daily = (move || yearly.get().powf(1.0 / 365.0)).into_signal();
+    let set_daily = (move |y: f64| set_yearly.set(y.powf(365.0))).into_signal_setter();
 
-    // let (daily_amount, set_daily_amount) = create_signal("".to_string());
-    // let (monthly_amount, set_monthly_amount) = create_signal("".to_string());
-    // let (yearly_amount, set_yearly_amount) = create_signal("".to_string());
-    // let (yearly_5_amount, set_yearly_5_amount) = create_signal("".to_string());
-    // let (yearly_10_amount, set_yearly_10_amount) = create_signal("".to_string());
+    let monthly = (move || yearly.get().powf(1.0 / 12.0)).into_signal();
+    let set_monthly = (move |y: f64| set_yearly.set(y.powf(12.0))).into_signal_setter();
 
-    // let daily_expr = (move || daily.with(eval_expr).unwrap_or_default()).into_signal();
-    // let monthly_expr = (move || monthly.with(eval_expr).unwrap_or_default()).into_signal();
-    // let yearly_expr = (move || yearly.with(eval_expr).unwrap_or_default()).into_signal();
-    // let yearly_5_expr = (move || yearly_5.with(eval_expr).unwrap_or_default()).into_signal();
-    // let yearly_10_expr = (move || yearly_10.with(eval_expr).unwrap_or_default()).into_signal();
+    let yearly_5 = (move || yearly.get().powf(5.0)).into_signal();
+    let set_yearly_5 = (move |y: f64| set_yearly.set(y.powf(1.0 / 5.0))).into_signal_setter();
 
-    // let amount_external = (move || amount.get()).into_signal();
+    let yearly_10 = (move || yearly.get().powf(10.0)).into_signal();
+    let set_yearly_10 = (move |y: f64| set_yearly.set(y.powf(1.0 / 10.0))).into_signal_setter();
 
-    // let (base_daily, set_base_daily) = create_signal(0.0f64);
-    // let (base_monthly, set_base_monthly) = create_signal(0.0f64);
-    // let (base_yearly, set_base_yearly) = create_signal(0.0f64);
-    // let (base_yearly_5, set_base_yearly_5) = create_signal(0.0f64);
-    // let (base_yearly_10, set_base_yearly_10) = create_signal(0.0f64);
+    let daily_amount = (move || daily.get() * amount.get()).into_signal();
+    let set_daily_amount = (move |a: f64| set_amount.set(a / daily.get())).into_signal_setter();
 
-    // let daily_amount_external =
-    //     (move || (base_daily.get() * calculated_amount.get()).to_string()).into_signal();
-    // let monthly_amount_external =
-    //     (move || (base_monthly.get() * calculated_amount.get()).to_string()).into_signal();
-    // let yearly_amount_external =
-    //     (move || (base_yearly.get() * calculated_amount.get()).to_string()).into_signal();
-    // let yearly_5_amount_external =
-    //     (move || (base_yearly_5.get() * calculated_amount.get()).to_string()).into_signal();
-    // let yearly_10_amount_external =
-    //     (move || (base_yearly_10.get() * calculated_amount.get()).to_string()).into_signal();
+    let monthly_amount = (move || monthly.get() * amount.get()).into_signal();
+    let set_monthly_amount = (move |a: f64| set_amount.set(a / monthly.get())).into_signal_setter();
 
-    // create_effect(move |_| {
-    //     let rate = (daily_expr.get() + 100.0) / 100.0;
-    //     set_base_monthly(rate.powf(30.0));
-    //     set_base_yearly(rate.powf(365.0));
-    //     set_base_yearly_5(rate.powf(5.0 * 365.0));
-    //     set_base_yearly_10(rate.powf(10.0 * 365.0));
-    // });
-    // create_effect(move |_| {
-    //     let rate = (monthly_expr.get() + 100.0) / 100.0;
-    //     set_base_daily(rate.powf(1.0 / 30.0));
-    //     set_base_yearly(rate.powf(12.0));
-    //     set_base_yearly_5(rate.powf(5.0 * 12.0));
-    //     set_base_yearly_10(rate.powf(10.0 * 12.0));
-    // });
-    // create_effect(move |_| {
-    //     let rate = (yearly_expr.get() + 100.0) / 100.0;
-    //     set_base_daily(rate.powf(1.0 / 365.0));
-    //     set_base_monthly(rate.powf(1.0 / 12.0));
-    //     set_base_yearly_5(rate.powf(5.0));
-    //     set_base_yearly_10(rate.powf(10.0));
-    // });
-    // create_effect(move |_| {
-    //     let rate = (yearly_5_expr.get() + 100.0) / 100.0;
-    //     set_base_daily(rate.powf(1.0 / 5.0 / 365.0));
-    //     set_base_monthly(rate.powf(1.0 / 5.0 / 12.0));
-    //     set_base_yearly(rate.powf(1.0 / 5.0));
-    //     set_base_yearly_10(rate.powf(2.0));
-    // });
-    // create_effect(move |_| {
-    //     let rate = (yearly_10_expr.get() + 100.0) / 100.0;
-    //     set_base_daily(rate.powf(1.0 / 10.0 / 365.0));
-    //     set_base_monthly(rate.powf(1.0 / 10.0 / 12.0));
-    //     set_base_yearly(rate.powf(1.0 / 10.0));
-    //     set_base_yearly_5(rate.powf(1.0 / 2.0));
-    // });
+    let yearly_amount = (move || yearly.get() * amount.get()).into_signal();
+    let set_yearly_amount = (move |a| set_amount.set(a / yearly.get())).into_signal_setter();
 
-    // let daily_external = (move || ((base_daily.get() - 1.0) * 100.0).to_string()).into_signal();
-    // let monthly_external = (move || ((base_monthly.get() - 1.0) * 100.0).to_string()).into_signal();
-    // let yearly_external = (move || ((base_yearly.get() - 1.0) * 100.0).to_string()).into_signal();
-    // let yearly_5_external =
-    //     (move || ((base_yearly_5.get() - 1.0) * 100.0).to_string()).into_signal();
-    // let yearly_10_external =
-    //     (move || ((base_yearly_10.get() - 1.0) * 100.0).to_string()).into_signal();
+    let yearly_5_amount = (move || yearly_5.get() * amount.get()).into_signal();
+    let set_yearly_5_amount =
+        (move |a: f64| set_amount.set(a / yearly_5.get())).into_signal_setter();
+
+    let yearly_10_amount = (move || yearly_10.get() * amount.get()).into_signal();
+    let set_yearly_10_amount =
+        (move |a: f64| set_amount.set(a / yearly_10.get())).into_signal_setter();
 
     view! {
         <div class="line">
             <Controlled label="Amount" description="Amount to multiply by the interest" value=amount.into() set_value=set_amount.into() />
             <Uncontrolled label="Calculated amount" description="Calculate value of the amount" value=amount.into()/>
         </div>
-        // <div class="line">
-        //     <Controlled label="Daily" description="Daily interest rate in %" value=daily external=daily_external set_value=set_daily/>
-        //     <Controlled label="Daily amount" description="Amount earned in a day." value=daily_amount external=daily_amount_external set_value=set_daily_amount />
-        // </div>
-        // <div class="line">
-        //     <Controlled label="Monthly" description="Monthly interest rate in %" value=monthly external=monthly_external set_value=set_monthly />
-        //     <Controlled label="Monthly amount" description="Amount earned in a month." value=monthly_amount external=monthly_amount_external set_value=set_monthly_amount />
-        // </div>
+        <div class="line">
+            <Controlled label="Daily" description="Daily interest rate in %" value=daily set_value=set_daily />
+            <Controlled label="Daily amount" description="Amount earned in a day." value=daily_amount set_value=set_daily_amount />
+        </div>
+        <div class="line">
+            <Controlled label="Monthly" description="Monthly interest rate in %" value=monthly set_value=set_monthly />
+            <Controlled label="Monthly amount" description="Amount earned in a month." value=monthly_amount set_value=set_monthly_amount />
+        </div>
         <div class="line">
             <Controlled label="Yearly" description="Yearly interest rate in %" value=yearly.into() set_value=set_yearly.into() />
-            <Controlled label="Yearly amount" description="Amount earned in a year." value=(move || { yearly.get() * amount.get()}).into_signal() set_value=(move |a| set_amount.set(a / yearly.get())).into_signal_setter() />
+            <Controlled label="Yearly amount" description="Amount earned in a year." value=yearly_amount set_value=set_yearly_amount />
         </div>
-        // <div class="line">
-        //     <Controlled label="5 years" description="5 years interest rate in %" value=yearly_5 external=yearly_5_external set_value=set_yearly_5 />
-        //     <Controlled label="5 years amount" description="Amount earned in 5 years." value=yearly_5_amount external=yearly_5_amount_external set_value=set_yearly_5_amount />
-        // </div>
-        // <div class="line">
-        //     <Controlled label="10 years" description="10 years interest rate in %" value=yearly_10 external=yearly_10_external set_value=set_yearly_10 />
-        //     <Controlled label="10 years amount" description="Amount earned in 10 years." value=yearly_10_amount external=yearly_10_amount_external set_value=set_yearly_10_amount />
-        // </div>
+        <div class="line">
+            <Controlled label="5 years" description="5 years interest rate in %" value=yearly_5 set_value=set_yearly_5 />
+            <Controlled label="5 years amount" description="Amount earned in 5 years." value=yearly_5_amount set_value=set_yearly_5_amount />
+        </div>
+        <div class="line">
+            <Controlled label="10 years" description="10 years interest rate in %" value=yearly_10 set_value=set_yearly_10 />
+            <Controlled label="10 years amount" description="Amount earned in 10 years." value=yearly_10_amount set_value=set_yearly_10_amount />
+        </div>
     }
 }
 
